adaf0c016f8f138bb9fc1f13f0ea11922357aeb2
“kirstenpomales” <kirstenpomales@gmail.com>
2025-05-27 18:40:10 +0100

    CTA

--- src/components/newcta.tsx
+++ src/components/newcta.tsx
@@ -10,10 +10,10 @@ export function Newcta() {
       <div className="container">
         <div className="relative mx-2 flex flex-col items-center gap-6 border border-t-0 pt-20 md:mx-10">
           <h3 className="max-w-3xl text-balance text-center font-heading text-3xl font-semibold tracking-tight sm:text-4xl md:text-5xl">
-            Your intern is excited to meet you!
+            Grow a following in your sleep!
           </h3>
           <p className="max-w-md text-center text-lg text-muted-foreground sm:text-xl">
-            Onboard an intern and boost your organic engagement on X
+            Automate your account and generate organic engagement on X
           </p>
           <div className="mb-16 flex grid-cols-2 items-center gap-4">
             <Button asChild size="lg" className="gap-2 font-semibold">
